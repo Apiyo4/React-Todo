ced49ab9a08c4b205d1541a0be3deefc04b30e9a
Annette Odingo <odingoannette@gmail.com>
2019-12-02 14:08:49 +0000

    Declare a todolist in App.js and pass it down to do list component and see if data is passed to it

--- src/App.js
+++ src/App.js
@@ -1,6 +1,29 @@
 import React, {useState} from 'react';
 import TodoForm from './components/TodoComponents/TodoForm';
+import TodoList from './components/TodoComponents/TodoList';
 
+let todoList = [
+  {
+    task: 'Organize Garage',
+    id: 1528817077286,
+    completed: false
+  },
+  {
+    task: 'Bake Cookies',
+    id: 1528817084358,
+    completed: false
+  },
+  {
+    task: 'Do laundry',
+    id: 1528839077286,
+    completed: false
+  },
+  {
+    task: 'Finish stretch goals',
+    id: 1528817077220,
+    completed: false
+  },
+];
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
@@ -23,10 +46,13 @@ onValueChange = (event)=>{
       inputTodo: value
   })
 }
+
   render() {
     return (
       <div>
+        
         <h2>Welcome to your Todo App!</h2>
+        <TodoList todoList= {todoList} />
         <TodoForm  
         inputTodo = {this.state.inputTodo}
         addTodo = {this.addTodo}
--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,20 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+import React, {Component} from 'react';
+
+
+export default class TodoList extends Component{
+    
+    render(){
+        return(
+            <div>
+         { console.log(this.props.todoList)}
+          {this.props.todoList.map(todo =>{
+            return <p>{todo.task}</p>;
+          })}
+            
+            </div>
+        )
+    }
+
+}
